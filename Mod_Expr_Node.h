21f6d2373c9b787984d04511ae3221b28b51af2b
datsimps <136854415+datsimps@users.noreply.github.com>
2023-09-27 16:10:12 -0400

    Update Mod_Expr_Node.h

--- Mod_Expr_Node.h
+++ Mod_Expr_Node.h
@@ -6,13 +6,11 @@
 class Mod_Expr_Node : public Binary_Expr_Node { 
 Expr_Node * left_;
 Expr_Node * right_;
-int leftValue;
-int rightValue;
 int precVal;
 
 public: 
   Mod_Expr_Node (Expr_Node* &num1, Expr_Node* &num2);
-
+  ~Mod_Expr_Node(void);
   int eval (void) override;
 };
 #endif  // !defined _MOD_EXPR_NODE_H_
