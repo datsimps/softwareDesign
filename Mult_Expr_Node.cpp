b76e3e4efdfe48cc4f1330ff97ba00bed5be4509
datsimps <136854415+datsimps@users.noreply.github.com>
2023-09-27 16:10:31 -0400

    Update Mult_Expr_Node.cpp

--- Mult_Expr_Node.cpp
+++ Mult_Expr_Node.cpp
@@ -10,6 +10,12 @@ Mult_Expr_Node::Mult_Expr_Node(Expr_Node* &left, Expr_Node* &right)
 ,precVal(2)
 { 
 }
+Mult_Expr_Node::~Mult_Expr_Node (void){
+  this->left_ = NULL;
+  this->right_= NULL;
+  delete this->left_;
+  delete this->right_;
+}
 int Mult_Expr_Node::eval()
 { 
   int leftValue = left_->eval();
