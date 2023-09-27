2389d69c07d82fd3966ffa8265119eea93382975
datsimps <136854415+datsimps@users.noreply.github.com>
2023-09-27 16:10:52 -0400

    Update Mult_Expr_Node.h

--- Mult_Expr_Node.h
+++ Mult_Expr_Node.h
@@ -6,13 +6,11 @@
 class Mult_Expr_Node : public Binary_Expr_Node { 
 Expr_Node * left_;
 Expr_Node * right_;
-int leftValue;
-int rightValue;
 int precVal;
 
 public: 
   Mult_Expr_Node (Expr_Node* &num1, Expr_Node* &num2);
-
+  ~Mult_Expr_Node(void);
   int eval (void) override;
 };
 #endif  // !defined _MULT_EXPR_NODE_H_
