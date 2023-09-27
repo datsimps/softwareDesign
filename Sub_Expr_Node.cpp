a52614e69a471a83812c73e1a624f1e1e92cdc97
datsimps <136854415+datsimps@users.noreply.github.com>
2023-09-27 16:11:27 -0400

    Update Sub_Expr_Node.cpp

--- Sub_Expr_Node.cpp
+++ Sub_Expr_Node.cpp
@@ -10,6 +10,12 @@ Sub_Expr_Node::Sub_Expr_Node(Expr_Node* &left, Expr_Node* &right)
 ,precVal(1)
 { 
 }
+Sub_Expr_Node::~Sub_Expr_Node (void){
+  this->left_ = NULL;
+  this->right_= NULL;
+  delete this->left_;
+  delete this->right_;
+}
 int Sub_Expr_Node::eval()
 { 
   int leftValue = left_->eval();
