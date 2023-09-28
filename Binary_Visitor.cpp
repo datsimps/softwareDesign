b5ce432d14422d24f1c8daffc57fdf4e005a1520
datsimps <136854415+datsimps@users.noreply.github.com>
2023-09-28 16:02:13 -0400

    Update Binary_Visitor.cpp

--- Binary_Visitor.cpp
+++ Binary_Visitor.cpp
@@ -1,11 +1,10 @@
 #include "Binary_Visitor.h"
-
-
 #include <iostream>
 
 Binary_Visitor::Binary_Visitor(void)
 { 
 }
+
 void Binary_Visitor::visit(Add_Expr_Node &b)
 { 
   std::cout << "Guests, like fish, begin to smell after three days... " << std::endl;
