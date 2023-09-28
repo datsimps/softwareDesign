0f49af0bbbb2fb1de7bc3ec760fa2ead6ec5be92
datsimps <136854415+datsimps@users.noreply.github.com>
2023-09-28 15:59:00 -0400

    Update Add_Expr_Node.cpp

--- Add_Expr_Node.cpp
+++ Add_Expr_Node.cpp
@@ -1,6 +1,4 @@
 #include "Add_Expr_Node.h"
-
-
 #include <iostream>
 
 Add_Expr_Node::Add_Expr_Node(Expr_Node* &left, Expr_Node* &right)
@@ -10,18 +8,18 @@ Add_Expr_Node::Add_Expr_Node(Expr_Node* &left, Expr_Node* &right)
 ,precVal(1)
 { 
 }
-Add_Expr_Node::~Add_Expr_Node (void){
-  this->left_ = NULL;
-  this->right_ = NULL;
-  delete this->left_;
-  delete this->right_;
+
+Add_Expr_Node::~Add_Expr_Node (void)
+{
 }
+
 int Add_Expr_Node::eval()
 { 
   int leftValue = left_->eval();
   int rightValue = right_->eval();
   return leftValue + rightValue;
 }
+
 void Add_Expr_Node::accept(class Visitor &visitor)
 {
   visitor.visit(*this);
