f87c79533f8f7d85f62c77e34c56bc378efd8718
datsimps <136854415+datsimps@users.noreply.github.com>
2023-09-28 16:18:34 -0400

    Update Add_Expr_Node.h

--- Add_Expr_Node.h
+++ Add_Expr_Node.h
@@ -5,13 +5,17 @@
 #include "Visitor.h"
 
 class Add_Expr_Node : public Binary_Expr_Node { 
+protected:
+
 Expr_Node * left_;
 Expr_Node * right_;
 int precVal;
+
 public: 
-  Add_Expr_Node (Expr_Node* &num1, Expr_Node* &num2);
-  ~Add_Expr_Node(void);
-  int eval (void) override;
-  void accept(class Visitor &visitor);
+  Binary_Expr_Node (void);
+  Binary_Expr_Node (Expr_Node* left, Expr_Node* right);
+  virtual int eval (void) =0;
+  virtual ~Binary_Expr_Node(void);
+
 };
 #endif  // !defined _ADD_EXPR_NODE_H_
