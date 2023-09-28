45a8865e47c36638362e987b4e65bc77acecd077
datsimps <136854415+datsimps@users.noreply.github.com>
2023-09-28 16:20:44 -0400

    Update Div_Expr_Node.h

--- Div_Expr_Node.h
+++ Div_Expr_Node.h
@@ -4,12 +4,16 @@
 #include "Binary_Expr_Node.h"
 
 class Div_Expr_Node : public Binary_Expr_Node { 
+protected:
+
 Expr_Node * left_;
 Expr_Node * right_;
 int precVal;
+
 public: 
   Div_Expr_Node (Expr_Node* &num1, Expr_Node* &num2);
   ~Div_Expr_Node(void);
   int eval (void) override;
+
 };
 #endif  // !defined _DIV_EXPR_NODE_H_
