d129d75915e833c5076078a06b5bdec92a8ccaa9
datsimps <136854415+datsimps@users.noreply.github.com>
2023-09-28 16:03:15 -0400

    Update Binary_Visitor.h

--- Binary_Visitor.h
+++ Binary_Visitor.h
@@ -4,11 +4,11 @@
 #include "Visitor.h"
 #include "Add_Expr_Node.h"
 
-
 class Binary_Visitor : public Visitor { 
 public: 
-  Binary_Visitor (void);
-
+  Binary_Visitor(void);
+  ~Binary_Visitor(void);
   virtual void visit (Add_Expr_Node &b);
 };
+
 #endif  // !defined _BINARY_VISITOR_H_
