fa59c8658f8691beca558932ea3e69c783b06c8a
datsimps <136854415+datsimps@users.noreply.github.com>
2023-09-28 16:01:22 -0400

    Update Binary_Expr_Node.cpp

--- Binary_Expr_Node.cpp
+++ Binary_Expr_Node.cpp
@@ -1,7 +1,5 @@
 #include "Binary_Expr_Node.h"
 #include "Visitor.h"
-
-
 #include <iostream>
 
 Binary_Expr_Node::Binary_Expr_Node (void)
@@ -9,23 +7,28 @@ Binary_Expr_Node::Binary_Expr_Node (void)
 ,right_(NULL)
 ,precVal(0)
 {
-
 }
+
 Binary_Expr_Node::Binary_Expr_Node (Expr_Node* left, Expr_Node* right)
 :left_(left)
 ,right_(right)
 ,precVal(1)
 { 
 }
+
 int Binary_Expr_Node::eval()
 { 
   int leftValue = left_->eval();
   int rightValue = right_->eval();
   return leftValue + rightValue;
 }
+
 Binary_Expr_Node::~Binary_Expr_Node (void)
 {
-
+  this->left_ = NULL;
+  this->right_ = NULL;
+  delete this->left_;
+  delete this->right_;
 }
 /*
 int main(){
