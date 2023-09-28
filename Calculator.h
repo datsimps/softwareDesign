6f0a0c68557d43a16940834dd408e61097c60807
datsimps <136854415+datsimps@users.noreply.github.com>
2023-09-28 16:52:16 -0400

    Update Calculator.h

--- Calculator.h
+++ Calculator.h
@@ -8,7 +8,7 @@
 class Calculator{ 
 public: 
 Calculator (Expr_Tree_Builder & builder);
-
+~Calculator(void);
 bool tree_Expr (const std::string inputExpr, InputCop cop);
 void Start(void);
 Expr_Tree_Builder* builder;
