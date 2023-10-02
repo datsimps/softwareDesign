ab5f8a946aef3c04bffd0e6b86f5195cd08c0220
datsimps <136854415+datsimps@users.noreply.github.com>
2023-10-02 16:22:42 -0400

    Update Div_Expr_Node.cpp

--- Div_Expr_Node.cpp
+++ Div_Expr_Node.cpp
@@ -3,19 +3,29 @@
 
 #include <iostream>
 
+//
+// Div_Expr_Node default constructor
+//
 Div_Expr_Node::Div_Expr_Node(Expr_Node* &left, Expr_Node* &right)
 :Binary_Expr_Node (left, right)
 ,left_(left)
 ,right_(right)
-,precVal(2)
 { 
 }
+
+//
+// Destructor.
+//
 Div_Expr_Node::~Div_Expr_Node (void){
   this->left_ = NULL;
   this->right_= NULL;
   delete this->left_;
   delete this->right_;
 }
+
+//
+// Evaluate
+//
 int Div_Expr_Node::eval()
 { 
   int leftValue = left_->eval();
@@ -26,4 +36,3 @@ int Div_Expr_Node::eval()
     return rightValue / leftValue;
   }
 }
-  
