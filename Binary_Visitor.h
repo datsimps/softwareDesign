7a1d70a5dbfbbebd8e3281c632169468c0842374
datsimps <136854415+datsimps@users.noreply.github.com>
2023-10-02 16:18:15 -0400

    Update Binary_Visitor.h

--- Binary_Visitor.h
+++ Binary_Visitor.h
@@ -4,10 +4,28 @@
 #include "Visitor.h"
 #include "Add_Expr_Node.h"
 
+  /**
+   * Parent class Visitor
+   * 
+   */
 class Binary_Visitor : public Visitor { 
 public: 
+
+  /**
+   * Initializing constructor.
+   *
+   */
   Binary_Visitor(void);
+
+  /// Destructor.
   ~Binary_Visitor(void);
+
+  /**
+   * visit fuction to add new functionality
+   *
+   * @param[in]      Add_Expr_Node    add node to visit
+   *
+   */
   virtual void visit (Add_Expr_Node &b);
 };
 
