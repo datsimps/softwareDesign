e95744a8c583a1d1d692c8604b9b64bcc149761f
datsimps <136854415+datsimps@users.noreply.github.com>
2023-10-02 16:50:08 -0400

    Update Sub_Expr_Node.h

--- Sub_Expr_Node.h
+++ Sub_Expr_Node.h
@@ -3,16 +3,40 @@
 
 #include "Binary_Expr_Node.h"
 
+  /**
+   * Parent class Binary_Expr_Node
+   * 
+  */
 class Sub_Expr_Node : public Binary_Expr_Node { 
 protected:
 
+  /**
+   * Protected member variables
+   * left and right
+   * 
+  */
 Expr_Node * left_;
 Expr_Node * right_;
 int precVal;
 
 public: 
+
+  /**
+   * Initializing constructor.
+   *
+   * @param[in]      Expr_Node*    Ptr to left node
+   * @param[in]      Expr_Node*    Ptr to right node
+   *
+  */
   Sub_Expr_Node (Expr_Node* &num1, Expr_Node* &num2);
+
+  /// Destructor.
   ~Sub_Expr_Node(void);
+
+  /**
+   * Evaluate, subtract the data from Expr_Node ptrs
+   *
+  */
   int eval (void) override;
 
 };
