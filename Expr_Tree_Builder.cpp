4829b4d73215278ff32974aa27ce2f1d013bcf20
datsimps <136854415+datsimps@users.noreply.github.com>
2023-10-02 16:34:03 -0400

    Update Expr_Tree_Builder.cpp

--- Expr_Tree_Builder.cpp
+++ Expr_Tree_Builder.cpp
@@ -1,22 +1,35 @@
 #include "Expr_Tree_Builder.h"
 
-
+//
+// Expr_Tree_Builder default constructor
+//
 Expr_Tree_Builder::Expr_Tree_Builder (void)
 {
    this->tree_ = new Expr_Tree();
 }; 
+
+//
+// Destructor.
+//
   Expr_Tree_Builder::~Expr_Tree_Builder (void){
     this->tree_->~Expr_Tree();
     this->tree_ = NULL;
     delete tree_;
   };
 
+//
+// build_number
+//
   void Expr_Tree_Builder::build_number (int n){
     Expr_Node * n1 = new Number_Node (n);
     tree_->insert(n1);
     n1 = NULL;
     delete n1;
   }; 
+
+//
+// build_add_operator
+//
   void Expr_Tree_Builder::build_add_operator (void){
     Expr_Node * e1 = tree_->stack.pop();
     Expr_Node * e2 = tree_->stack.pop();
@@ -29,6 +42,10 @@ Expr_Tree_Builder::Expr_Tree_Builder (void)
     delete e2;
     delete expr;
   }; 
+
+//
+// build_subtract_operator
+//
   void Expr_Tree_Builder::build_subtract_operator (void){
     Expr_Node * e1 = tree_->stack.pop();
     Expr_Node * e2 = tree_->stack.pop();
@@ -41,6 +58,11 @@ Expr_Tree_Builder::Expr_Tree_Builder (void)
     delete e2;
     delete expr;
   };
+
+
+//
+// build_mult_operator
+//
   void Expr_Tree_Builder::build_mult_operator (void){
     Expr_Node * e1 = tree_->stack.pop();
     Expr_Node * e2 = tree_->stack.pop();
@@ -53,6 +75,10 @@ Expr_Tree_Builder::Expr_Tree_Builder (void)
     delete e2;
     delete expr;
   };
+
+//
+// build_div_operator
+//
   void Expr_Tree_Builder::build_div_operator (void){
     Expr_Node * e1 = tree_->stack.pop();
     Expr_Node * e2 = tree_->stack.pop();
@@ -65,6 +91,10 @@ Expr_Tree_Builder::Expr_Tree_Builder (void)
     delete e2;
     delete expr;
   };
+
+//
+// build_mod_operator
+//
   void Expr_Tree_Builder::build_mod_operator (void){
     Expr_Node * e1 = tree_->stack.pop();
     Expr_Node * e2 = tree_->stack.pop();
@@ -77,6 +107,10 @@ Expr_Tree_Builder::Expr_Tree_Builder (void)
     delete e2;
     delete expr;
   };
+
+//
+// getAnswer
+//
   void Expr_Tree_Builder::getAnswer(void){
     Expr_Node * e1 = tree_->stack.pop();
     int result = e1->eval();
@@ -85,18 +119,4 @@ Expr_Tree_Builder::Expr_Tree_Builder (void)
     e1 = NULL;
     delete e1;
   }
-Expr_Tree * Expr_Tree_Builder::get_expression (void) 
-{ 
-  return this->tree_; 
-}
-/*
-void Expr_Tree_Builder::accept(class Visitor &visitor)
-{
-  visitor.visit(*this);
-  std::cout << "this worked" << std::endl;
-  Expr_Node *expr2 = returnPtr();
-  int result = expr2->eval ();
-  std::cout << result << std::endl;
-  std::cout << "Bow before Zuul: " << std::endl;
-}
-*/
+
