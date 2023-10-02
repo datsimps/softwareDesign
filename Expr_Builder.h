9b2839b4412c9c476f96761d60aa286ba9df79bd
datsimps <136854415+datsimps@users.noreply.github.com>
2023-10-02 16:24:00 -0400

    Update Expr_Builder.h

--- Expr_Builder.h
+++ Expr_Builder.h
@@ -4,11 +4,51 @@
 class Expr_Builder { 
 public: 
 
-  virtual void build_number (int n) = 0; 
+  /**
+   * Initializing constructor.
+   *
+  */
+  Expr_Builder(void){};
+
+  /// Destructor.
+  virtual ~Expr_Builder(void){};
+
+  /**
+   * build_number, pure virtual to be overwritten
+   *
+   * @param[in]      int value    set value to number
+   *
+  */
+  virtual void build_number (int n) = 0;
+
+  /**
+   * build_add_operator, pure virtual to be overwritten
+   *
+  */
   virtual void build_add_operator (void) = 0; 
+
+  /**
+   * build_subtract_operator, pure virtual to be overwritten
+   *
+  */
   virtual void build_subtract_operator (void) = 0;
+
+  /**
+   * build_mult_operator, pure virtual to be overwritten
+   *
+  */
   virtual void build_mult_operator (void) = 0;
+
+  /**
+   * build_div_operator, pure virtual to be overwritten
+   *
+  */
   virtual void build_div_operator (void) = 0;
+
+  /**
+   * build_mod_operator, pure virtual to be overwritten
+   *
+  */
   virtual void build_mod_operator (void) = 0;
 
 };
