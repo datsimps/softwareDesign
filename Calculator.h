52c92b7bcbda8b05072c814f881786657a00c8d9
datsimps <136854415+datsimps@users.noreply.github.com>
2023-10-02 16:22:04 -0400

    Update Calculator.h

--- Calculator.h
+++ Calculator.h
@@ -6,10 +6,44 @@
 
 
 class Calculator{ 
-public: 
-Calculator (Expr_Tree_Builder & builder);
-~Calculator(void);
-bool tree_Expr (const std::string inputExpr, InputCop cop);
-void Start(void);
+protected:
+
+ /**
+   * Protected member variable
+   * builder
+   * 
+  */
 Expr_Tree_Builder* builder;
+
+public: 
+
+  /**
+   * Initializing constructor.
+   *
+   * @param[in]      Expr_Tree_Builder    builder to build operations
+   *
+  */
+Calculator (Expr_Tree_Builder & builder);
+
+/// Destructor.
+~Calculator(void);
+
+  /**
+   * Instructs the builder to only build necessary nodes
+   *
+   * @param[in]      string      input expression to use
+   * @param[in]      InputCop    input monitor for valid ops
+   *
+   * @return         bool                 for while loop usage
+   *
+  */
+bool tree_Expr (const std::string inputExpr, InputCop cop);
+
+  /**
+   * Main function to user menu, call the tree_exor function, output answer
+   *
+   *
+  */
+void Start(void);
+
 };
