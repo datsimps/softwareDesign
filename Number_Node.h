ac03c6f8fee9b85f15a8261720d7ee7d371a8f49
datsimps <136854415+datsimps@users.noreply.github.com>
2023-10-02 16:48:40 -0400

    Update Number_Node.h

--- Number_Node.h
+++ Number_Node.h
@@ -3,11 +3,39 @@
 
 #include "Unary_Expr_Node.h"
 
+  /**
+   * Parent class Unary_Expr_Node
+   * 
+  */
 class Number_Node : public Unary_Expr_Node { 
-public: 
-  Number_Node (int value);
-  int eval (void);
+protected:
+
+  /**
+   * Protected member variable
+   * number
+   * 
+  */
+
   int number;
+public: 
+
+  /**
+   * Initializing constructor.
+   *
+   * @param[in]      int value    set value to number
+   *
+  */
+  Number_Node (int value);
+
+  /// Destructor.
+  ~Number_Node (void);
+
+  /**
+   * Evaluate, return the number of node
+   *
+  */
+  int eval (void);
+
 };
 
 #endif  // !defined _NUMBER_NODE_H_
