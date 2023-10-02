3d134ff5723d67e8458dda79daa16b2c9d6c558b
datsimps <136854415+datsimps@users.noreply.github.com>
2023-10-02 16:14:19 -0400

    Update Binary_Expr_Node.cpp

--- Binary_Expr_Node.cpp
+++ Binary_Expr_Node.cpp
@@ -2,27 +2,27 @@
 #include "Visitor.h"
 #include <iostream>
 
+//
+// Binary_Expr_Node default constructor
+//
 Binary_Expr_Node::Binary_Expr_Node (void)
 :left_(NULL)
 ,right_(NULL)
-,precVal(0)
 {
 }
 
+//
+// Binary_Expr_Node constructor
+//
 Binary_Expr_Node::Binary_Expr_Node (Expr_Node* left, Expr_Node* right)
 :left_(left)
 ,right_(right)
-,precVal(1)
 { 
 }
 
-int Binary_Expr_Node::eval()
-{ 
-  int leftValue = left_->eval();
-  int rightValue = right_->eval();
-  return leftValue + rightValue;
-}
-
+//
+// Destructor
+//
 Binary_Expr_Node::~Binary_Expr_Node (void)
 {
   this->left_ = NULL;
@@ -30,9 +30,13 @@ Binary_Expr_Node::~Binary_Expr_Node (void)
   delete this->left_;
   delete this->right_;
 }
-/*
-int main(){
 
-  return 0;
+//
+// Evalutate
+//
+int Binary_Expr_Node::eval()
+{ 
+  int leftValue = left_->eval();
+  int rightValue = right_->eval();
+  return leftValue + rightValue;
 }
-*/
