2c14113a40ee2f547b4aab4b76362eaf4b51085f
datsimps <136854415+datsimps@users.noreply.github.com>
2023-10-02 16:17:21 -0400

    Update Binary_Visitor.cpp

--- Binary_Visitor.cpp
+++ Binary_Visitor.cpp
@@ -1,14 +1,23 @@
 #include "Binary_Visitor.h"
 #include <iostream>
 
+//
+// Binary_Visitor default constructor
+//
 Binary_Visitor::Binary_Visitor(void)
 { 
 }
 
+//
+// Derstructor.
+//
 Binary_Visitor::~Binary_Visitor(void)
 { 
 }
 
+//
+// Visit
+//
 void Binary_Visitor::visit(Add_Expr_Node &b)
 { 
   std::cout << "Guests, like fish, begin to smell after three days... " << std::endl;
