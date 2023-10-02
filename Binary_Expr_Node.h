6c3f511a0a1d756a9a3be5c8afdb2109a01f6770
datsimps <136854415+datsimps@users.noreply.github.com>
2023-10-02 16:16:44 -0400

    Update Binary_Expr_Node.h

--- Binary_Expr_Node.h
+++ Binary_Expr_Node.h
@@ -4,21 +4,49 @@
 #include "Expr_Node.h"
 #include "Visitor.h"
 
+
+  /**
+   * Parent class Expr_Node
+   * 
+  */
 class Binary_Expr_Node : public Expr_Node { 
 protected:
 
+ /**
+   * Protected member variables
+   * left and right
+   * 
+  */
 Expr_Node * left_;
 Expr_Node * right_;
-int precVal;
 
 public: 
+
+  /**
+   * Initializing constructor.
+   *
+  */
   Binary_Expr_Node (void);
+
+  /**
+   * Initializing constructor.
+   *
+   * @param[in]      Expr_Node*    Ptr to left node
+   * @param[in]      Expr_Node*    Ptr to right node
+   *
+  */
   Binary_Expr_Node (Expr_Node* left, Expr_Node* right);
-  virtual int eval (void) =0;
+
+  /// Destructor.
   virtual ~Binary_Expr_Node(void);
+
+  /**
+   * Evaluate, pure virtual to be overwritten
+   * 
+   * @return      int      Answer of evaluate
+  */
+  virtual int eval (void) =0;
 
 }; 
 
 #endif  // !defined _BINARY_EXPR_NODE_H_
-
-
