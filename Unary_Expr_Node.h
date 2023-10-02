b243423932e91f8477884e0fea75b7e1c7e66098
datsimps <136854415+datsimps@users.noreply.github.com>
2023-10-02 16:52:02 -0400

    Update Unary_Expr_Node.h

--- Unary_Expr_Node.h
+++ Unary_Expr_Node.h
@@ -3,16 +3,40 @@
 
 #include "Expr_Node.h"
 
-
+  /**
+   * Parent class Expr_Node
+   * 
+  */
 class Unary_Expr_Node : public Expr_Node {
+protected:
+
+  /**
+   * Protected member variable
+   * number
+   * 
+  */
 
   int number;
-           
+
 public:
 
+  /**
+   * Initializing constructor.
+   *
+   * @param[in]      int value    set value to number
+   *
+  */
   Unary_Expr_Node (int val);
 
-  int eval();
+  /// Destructor.
+  virtual ~Unary_Expr_Node (void);
+
+  /**
+   * Evaluate, return the number of node
+   * pure virtural to be overwritten
+   *
+  */
+  virtual int eval() =0;
 
 };
 #endif  // !defined _UNARY_EXPR_NODE_H_
