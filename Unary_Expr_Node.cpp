36f299bbe9e8a1658af251954da0db89ee433c07
datsimps <136854415+datsimps@users.noreply.github.com>
2023-10-02 16:50:40 -0400

    Update Unary_Expr_Node.cpp

--- Unary_Expr_Node.cpp
+++ Unary_Expr_Node.cpp
@@ -1,10 +1,24 @@
 #include "Unary_Expr_Node.h"
 #include <iostream>
 
+//
+// Unary_Expr_Node default constructor
+//
 Unary_Expr_Node::Unary_Expr_Node (int val)
 { 
   number = val;
 }
+
+//
+// Destructor
+//
+Unary_Expr_Node::~Unary_Expr_Node (void)
+{ 
+}
+
+//
+// Evaluate
+//
 int Unary_Expr_Node::eval()
 { 
 return number;
