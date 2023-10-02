e30ba77cee3a7554eb236342c338a661ace0f9bb
datsimps <136854415+datsimps@users.noreply.github.com>
2023-10-02 16:47:34 -0400

    Update Number_Node.cpp

--- Number_Node.cpp
+++ Number_Node.cpp
@@ -1,11 +1,25 @@
 #include "Number_Node.h"
 #include <iostream>
 
+//
+// Number_Node default constructor
+//
 Number_Node::Number_Node (int value)
-: Unary_Expr_Node(value)
+:Unary_Expr_Node(value)
 { 
   number = value;
 }
+
+//
+// Destructor
+//
+Number_Node::~Number_Node (void)
+{ 
+}
+
+//
+// Evaluate
+//
 int Number_Node::eval()
 { 
 return number;
