3b01987ddb54943d07a415e6246a9c6608dcd31f
datsimps <136854415+datsimps@users.noreply.github.com>
2023-10-02 16:28:27 -0400

    Update Expr_Tree.h

--- Expr_Tree.h
+++ Expr_Tree.h
@@ -5,16 +5,46 @@
 #include "Expr_Node.h"
 
 class Expr_Tree { 
-public:
-  Expr_Tree (void);
-  ~Expr_Tree(void);
-  void insert(Expr_Node* node);
-  void print(Array <Expr_Node *> &exeCommand);
+protected:
 
+  /**
+   * Protected member variables
+   * left, right, root
+   * stack of Expr_Node ptrs
+   * 
+  */
   Stack<Expr_Node*> stack;
   Expr_Node* left;
   Expr_Node* right;
   Expr_Node* root;
+
+public:
+
+  /**
+   * Initializing constructor.
+   *
+  */
+  Expr_Tree (void);
+
+  /// Destructor.
+  ~Expr_Tree(void);
+
+  /**
+   * insert node to tree
+   *
+   * @param[in]      Expr_Node    add node to tree
+   *
+  */
+  void insert(Expr_Node* node);
+
+  /**
+   * outputs tree via iterator
+   *
+   * @param[in]      Expr_Node    add node to tree
+   *
+  */
+  void print(Array <Expr_Node *> &exeCommand);
+
 };
 
 #endif  // !defined _EXPR_TREE_H_
